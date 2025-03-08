a2f473b19608e7c15c61dbd6f28cf34488e49fda
Bk Mutinda <kbmkyalo@gmail.com>
2025-03-09 00:35:30 +0300

    last

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,6 @@
 import "./App.css";
+import Contact from "./components/contacts/Contact";
+import Footer from "./components/contacts/footer/footer";
 import About from "./components/header/About/About";
 import Home from "./components/header/Home/Home";
 import Nav from "./components/header/nav";
@@ -14,6 +16,8 @@ function App() {
         <About />
         <Projects />
         < Resume />
+        <Contact />
+        <Footer />
       </div>
     </>
   );
---
+++ src/components/contacts/footer/Footer.jsx
@@ -0,0 +1,60 @@
+import React from "react";
+import "./footer.css";
+import {
+  FaFacebook,
+  FaGithub,
+  FaInstagram,
+  FaLinkedin,
+  FaPhoneAlt,
+  FaTwitter,
+} from "react-icons/fa";
+import { FaXTwitter } from "react-icons/fa6";
+import { MdEmail } from "react-icons/md";
+import { BsWhatsapp } from "react-icons/bs";
+
+const Footer = () => {
+  return (
+    <section>
+      <div className="">
+        <h3 className="w-full text-2xl border-b-2 border-gray-400">Find Me</h3>
+        <div className="flex justify-around">
+          <div className="">
+            <ul className="flex flex-col gap-5">
+              <li>
+                <FaLinkedin className="" />{" "}
+              </li>
+              <li>
+                {" "}
+                <FaGithub />{" "}
+              </li>
+              <li>
+                <FaFacebook />{" "}
+              </li>
+              <li>
+                <FaInstagram />{" "}
+              </li>
+              <li>
+                <FaXTwitter />{" "}
+              </li>
+            </ul>
+          </div>
+          <div className="">
+            <ul className="flex flex-col gap-5">
+              <li>
+                <MdEmail />{" "}
+              </li>
+              <li>
+                <FaPhoneAlt />
+              </li>
+              <li>
+                <BsWhatsapp />{" "}
+              </li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Footer;
